e47d191181b7ed95aa08d0dcab3a5d77c5086a34
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-06-04 09:04:19 +0200

    Adding 2 small delegate with events project

---
+++ Simple-Event/Simple-Event/Kontener.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Event
+{
+    class Kontener
+    {
+        private int? x;
+
+        public Kontener() 
+        {
+            SetX(0);
+        }
+
+        public delegate void EventHandler(Kontener k);
+
+        public static event EventHandler XJestUjemne;
+        public static event EventHandler XJestDodatnie;
+
+        public void SetX(int? x)
+        { 
+            this.x = x;
+            if (x < 0) 
+            {
+                if (x != null)
+                {
+                    XJestUjemne(this);
+                }
+            }
+
+            if (x > 0) 
+            {
+                if (x != null)
+                {
+                    XJestDodatnie(this);
+                }
+            }
+        }
+        public int? GetX()
+        {
+            return x;
+        }
+    }
+}
---
+++ Simple-Event/Simple-Event/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Simple_Event
+{
+    class Program
+    {
+            public static void OnUjemne(Kontener k) 
+            {
+                Console.WriteLine($"X jest ujemne = {k.GetX()}");
+            }
+            public static void OnDodatnie(Kontener k) 
+            {
+                Console.WriteLine($"X jest dodatnie = {k.GetX()}");
+            }
+        static void Main(string[] args)
+        {
+            Kontener kont = new Kontener();
+            Kontener.XJestUjemne += new Kontener.EventHandler(OnUjemne);
+            Kontener.XJestDodatnie += new Kontener.EventHandler(OnDodatnie);
+            
+            kont.SetX(2);
+            kont.SetX(-2);
+        }
+    }
+}
